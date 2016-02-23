93fb9c48dcf510283cd3c4d733b0814e9467e74e
Adam Gutierrez <hevyw8@gmail.com>
2016-02-22 22:14:01 -0600

    added videos scaffold

---
+++ app/controllers/videos_controller.rb
@@ -0,0 +1,55 @@
+class VideosController < ApplicationController
+  before_action :set_video, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @videos = Video.all
+  end
+
+  def show
+  end
+
+
+  def new
+    @video = Video.new
+  end
+
+
+  def edit
+  end
+
+  def create
+    @video = Video.new(video_params)
+    if @video.save
+      redirect_to @video, notice: 'Video was successfully created.'
+    else
+      render :new       
+    end   
+  end
+
+ 
+  def update
+    if @video.update(video_params)
+      redirect_to @video, notice: 'Video was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  
+  def destroy
+    @video.destroy
+      redirect_to videos_url, notice: 'Video was successfully destroyed.'
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_video
+      @video = Video.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def video_params
+      params.require(:video).permit(:description)
+    end
+end

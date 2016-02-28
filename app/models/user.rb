9536ce126b952dc23e88def72c40f9567c2a27a5
Adam Gutierrez <hevyw8@gmail.com>
2016-02-28 12:12:09 -0700

    added password validation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+ 	validates :password, :presence => true,
+           	:on => :create,
+         	  :format => {:with => /\A.*(?=.{8,}).*\Z/ }
 
   has_many :videos
 
